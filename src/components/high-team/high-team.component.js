b4491d5695d13be5e3891067bd3102606b787168
Alex Hedley <alex.hedley@me.com>
2022-11-13 18:43:56 +0000

    WIP: High Team

--- src/components/high-team/high-team.component.js
+++ src/components/high-team/high-team.component.js
@@ -6,6 +6,7 @@ angular.module("myApp").component("high-team", {
   templateUrl: "components/high-team/high-team.html",
   controller: HighTeamController,
   bindings: {
+    title: "<",
     teams: "<",
   },
 });
