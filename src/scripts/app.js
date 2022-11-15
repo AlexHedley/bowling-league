80b89e1964e0a6f5cbc222331c705b32c27167d7
Alex Hedley <alex.hedley@me.com>
2022-11-15 17:36:23 +0000

    Filtering on data

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,6 +12,17 @@ myApp.controller("myController", function ($scope, $http, $q, $filter) {
   $scope.highTeamSeriesHcp;
   $scope.fixtures;
 
+  $scope.leagueFiltered;
+  $scope.teamScoresFiltered;
+  $scope.highGameFiltered;
+  $scope.highSeriesFiltered;
+  $scope.highAverageFiltered;
+  $scope.highTeamGameScoreFiltered;
+  $scope.highTeamSeriesScoreFiltered;
+  $scope.highTeamGameHcpFiltered;
+  $scope.highTeamSeriesHcpFiltered;
+  $scope.fixturesFiltered;
+
   $scope.dates = [
     { key: "05/10/2022", value: "2022-10-05" },
     { key: "12/10/2022", value: "2022-10-12" },
@@ -22,12 +33,46 @@ myApp.controller("myController", function ($scope, $http, $q, $filter) {
     $scope.dateValue = $scope.chosenDate.value;
     // $scope.dateCode = $scope.chosenDate.key;
 
-    var leagueFile = "data/league.json";
-    $http.get(leagueFile).then(function (response) {
-      $scope.league = $filter("filter")(response.data, (item) => {
-        return item.date === $scope.dateValue;
-      })[0].teams;
-    });
+    $scope.leagueFiltered = $filter("filter")($scope.league, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].teams;
+
+    $scope.teamScoresFiltered = $filter("filter")($scope.teamScores, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].teams;
+
+    $scope.highGameFiltered = $filter("filter")($scope.highGame, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].players;
+
+    $scope.highSeriesFiltered = $filter("filter")($scope.highSeries, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].players;
+
+    $scope.highAverageFiltered = $filter("filter")($scope.highAverage, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].players;
+
+    $scope.highTeamGameScoreFiltered = $filter("filter")($scope.highTeamGameScore, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].teams;
+
+    $scope.highTeamSeriesScoreFiltered = $filter("filter")($scope.highTeamSeriesScore, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].teams;
+
+    $scope.highTeamGameHcpFiltered = $filter("filter")($scope.highTeamGameHcp, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].teams;
+
+    $scope.highTeamSeriesHcpFiltered = $filter("filter")($scope.highTeamSeriesHcp, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].teams;
+    
+    $scope.fixturesFiltered = $filter("filter")($scope.fixtures, (item) => {
+      return item.date === $scope.dateValue;
+    })[0].fixtures;
+    
   };
 
   $scope.init = function () {
@@ -35,54 +80,54 @@ myApp.controller("myController", function ($scope, $http, $q, $filter) {
   };
 
   getData = () => {
-    // var leagueFile = "data/league.json";
-    // $http.get(leagueFile).then(function (response) {
-    //   $scope.league = response.data.teams;
-    // });
+    var leagueFile = "data/league.json";
+    $http.get(leagueFile).then(function (response) {
+      $scope.league = response.data;
+    });
 
     var scoresFile = "data/scores.json";
     $http.get(scoresFile).then(function (response) {
-      $scope.teamScores = response.data.teams;
+      $scope.teamScores = response.data;
     });
 
     var highGameFile = "data/high-game.json";
     $http.get(highGameFile).then(function (response) {
-      $scope.highGame = response.data.players;
+      $scope.highGame = response.data;
     });
 
     var highSeriesFile = "data/high-series.json";
     $http.get(highSeriesFile).then(function (response) {
-      $scope.highSeries = response.data.players;
+      $scope.highSeries = response.data;
     });
 
     var highAverageFile = "data/high-average.json";
     $http.get(highAverageFile).then(function (response) {
-      $scope.highAverage = response.data.players;
+      $scope.highAverage = response.data;
     });
 
     var highTeamGameScoreFile = "data/high-team-game-score.json";
     $http.get(highTeamGameScoreFile).then(function (response) {
-      $scope.highTeamGameScore = response.data.teams;
+      $scope.highTeamGameScore = response.data;
     });
 
     var highTeamSeriesScoreFile = "data/high-team-series-score.json";
     $http.get(highTeamSeriesScoreFile).then(function (response) {
-      $scope.highTeamSeriesScore = response.data.teams;
+      $scope.highTeamSeriesScore = response.data;
     });
 
     var highTeamGameHcpFile = "data/high-team-game-hcp.json";
     $http.get(highTeamGameHcpFile).then(function (response) {
-      $scope.highTeamGameHcp = response.data.teams;
+      $scope.highTeamGameHcp = response.data;
     });
 
     var highTeamSeriesHcpFile = "data/high-team-series-hcp.json";
     $http.get(highTeamSeriesHcpFile).then(function (response) {
-      $scope.highTeamSeriesHcp = response.data.teams;
+      $scope.highTeamSeriesHcp = response.data;
     });
 
     var fixturesFile = "data/fixtures.json";
     $http.get(fixturesFile).then(function (response) {
-      $scope.fixtures = response.data.fixtures;
+      $scope.fixtures = response.data;
     });
   };
 
